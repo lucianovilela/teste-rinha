6cecf29975c180472e67c37692ece97c55e0a249
Luciano Vilela <lucianovilela@gmail.com>
2024-02-11 19:18:27 -0300

    versão funcionando

--- solucao/index.js
+++ solucao/index.js
@@ -59,41 +59,56 @@ app.get('/clientes/:id/extrato', async (req, res) => {
 });
 
 const validateRequest = (req) => {
-  const valor = parseInt(req.body.valor);
-  const descricao = req.body.descricao; 
-  if(valor === undefined || valor < 0 || descricao === undefined || descricao === null || descricao.length > 10  ) return false;
-  
+  try {
+    const valor = req.body.valor * 1;
+    const descricao = req.body.descricao;
+    const tipo = req.body.tipo;
+    if (
+      !Number.isInteger(valor) ||
+      valor === undefined ||
+      valor < 0 ||
+      descricao === undefined ||
+      descricao === null ||
+      descricao.length > 10 ||
+      descricao.length < 1) return false;
+    if (tipo !== "d" && tipo !== "c") return false;
+  }
+  catch (e) { return false; }
   return true;
 
 }
 
 app.post('/clientes/:id/transacoes', async (req, res) => {
-  if(!validateRequest(req)){
+  if (!validateRequest(req)) {
     return res.send(422).json();
   }
+  const clientConnect = await pool.connect();
   try {
-    await pool.query('BEGIN');
-    const client = await pool.query(`select c.limite, b.valor from clientes c inner join saldos b on c.id = b.cliente_id where c.id=$1 for update`, [req.params.id]);
+    await clientConnect.query('BEGIN');
+    const client = await clientConnect.query(`select c.limite, b.valor from clientes c inner join saldos b on c.id = b.cliente_id where c.id=$1 for update`, [req.params.id]);
     if (client.rowCount === 0) {
-      await pool.query('ROLLBACK').json();
+      await clientConnect.query('ROLLBACK').json();
       return res.status(404);
     }
     if (req.body.tipo === "d"
-      && Math.abs(client.rows[0].valor) < (req.body.valor * 1)) {
-      await pool.query('ROLLBACK');
+      && client.rows[0].valor + client.rows[0].limite < (req.body.valor * 1)) {
+      await clientConnect.query('ROLLBACK');
       return res.status(422).json();
     }
 
     const valor = req.body.tipo === "d" ? req.body.valor * -1 : req.body.valor * 1;
-    const novoSaldo = await pool.query('UPDATE saldos SET valor = valor + $2 where cliente_id = $1 RETURNING valor', [req.params.id, valor])
-    await pool.query('INSERT INTO transacoes(cliente_id, valor, tipo, descricao) values ($1, $2, $3, $4)',
+    const novoSaldo = await clientConnect.query('UPDATE saldos SET valor = valor + $2 where cliente_id = $1 RETURNING valor', [req.params.id, valor])
+    await clientConnect.query('INSERT INTO transacoes(cliente_id, valor, tipo, descricao) values ($1, $2, $3, $4)',
       [req.params.id, req.body.valor * 1, req.body.tipo, req.body.descricao]);
-    await pool.query('COMMIT');
+    await clientConnect.query('COMMIT');
     return res.status(200).json({ limite: client.rows[0].limite, saldo: novoSaldo.rows[0].valor })
   } catch (error) {
-    await pool.query('ROLLBACK');
+    await clientConnect.query('ROLLBACK');
     console.error('Erro ao buscar dados:', error);
     return res.status(500).json();
+  }
+  finally {
+    await clientConnect.release();
   }
 
 })
